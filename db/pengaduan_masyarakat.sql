c4675d620cecced742b515eb90fed7629d7577e2
Hafidz25 <hafidzalif80@gmail.com>
2023-03-23 07:42:15 +0700

    test

--- db/pengaduan_masyarakat.sql
+++ db/pengaduan_masyarakat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Des 2021 pada 03.08
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.4.9
+-- Waktu pembuatan: 21 Mar 2023 pada 03.20
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,14 @@ CREATE TABLE `masyarakat` (
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
   `telp` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
+('123456789', 'Asep', 'asep', 'dc855efb0dc7476760afaa1b281665f1', '123123');
 
 -- --------------------------------------------------------
 
@@ -48,7 +55,15 @@ CREATE TABLE `pengaduan` (
   `isi_laporan` text NOT NULL,
   `foto` varchar(255) NOT NULL,
   `status` enum('proses','selesai') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(1, '2023-03-21', '123456789', 'tes', '210320234231Login Siswa - SITEDI.png', 'selesai'),
+(2, '2023-03-21', '123456789', 'tes 2', '210320234448Screenshot 2022-12-13 132725.png', 'proses');
 
 -- --------------------------------------------------------
 
@@ -63,15 +78,15 @@ CREATE TABLE `petugas` (
   `password` varchar(255) NOT NULL,
   `telp_petugas` varchar(13) NOT NULL,
   `level` enum('admin','petugas') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `petugas`
 --
 
 INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp_petugas`, `level`) VALUES
-(1, 'Aqil Rahman', 'admin', '21232f297a57a5a743894a0e4a801fc3', '081215951492', 'admin'),
-(2, 'M Riski', 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', '081215951492', 'petugas');
+(1, 'Hafidz Alif', 'admin', '21232f297a57a5a743894a0e4a801fc3', '081215951492', 'admin'),
+(2, 'Firdaus', 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', '081215951492', 'petugas');
 
 -- --------------------------------------------------------
 
@@ -85,7 +100,14 @@ CREATE TABLE `tanggapan` (
   `tgl_tanggapan` varchar(20) NOT NULL,
   `tanggapan` text NOT NULL,
   `id_petugas` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `tanggapan`
+--
+
+INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(1, 1, '2023-03-21', 'tes tanggapan', 2);
 
 --
 -- Indexes for dumped tables
@@ -101,7 +123,8 @@ ALTER TABLE `masyarakat`
 -- Indeks untuk tabel `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  ADD PRIMARY KEY (`id_pengaduan`);
+  ADD PRIMARY KEY (`id_pengaduan`),
+  ADD KEY `nik` (`nik`);
 
 --
 -- Indeks untuk tabel `petugas`
@@ -113,7 +136,9 @@ ALTER TABLE `petugas`
 -- Indeks untuk tabel `tanggapan`
 --
 ALTER TABLE `tanggapan`
-  ADD PRIMARY KEY (`id_tanggapan`);
+  ADD PRIMARY KEY (`id_tanggapan`),
+  ADD KEY `id_pengaduan` (`id_pengaduan`),
+  ADD KEY `id_petugas` (`id_petugas`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -123,7 +148,7 @@ ALTER TABLE `tanggapan`
 -- AUTO_INCREMENT untuk tabel `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pengaduan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `petugas`
@@ -135,7 +160,24 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT untuk tabel `tanggapan`
 --
 ALTER TABLE `tanggapan`
-  MODIFY `id_tanggapan` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tanggapan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pengaduan`
+--
+ALTER TABLE `pengaduan`
+  ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `masyarakat` (`nik`);
+
+--
+-- Ketidakleluasaan untuk tabel `tanggapan`
+--
+ALTER TABLE `tanggapan`
+  ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`),
+  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
